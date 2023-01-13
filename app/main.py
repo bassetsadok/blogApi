cab6387f4c10c082b03068b2b9a4013b3a87d315
bassetsadok <50278427+bassetsadok@users.noreply.github.com>
2023-01-13 15:51:50 +0100

    adding orm, specify res schema, create userTable

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import psycopg2
-from typing import Optional
+from typing import Optional,List
 from fastapi import  FastAPI, Response, status,HTTPException,Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
 from psycopg2.extras import RealDictCursor
 import time
-from . import models
+from . import models,schemas
 from .database import  engine,get_db
 from sqlalchemy.orm import Session
 
@@ -14,11 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-
-class Post(BaseModel):
-    title:str
-    content:str
-    published:bool=True
 
 while True:
     try:
@@ -53,50 +48,63 @@ async def test_myposts(db: Session = Depends(get_db)):
     posts=db.query(models.Post).all()
     return {"data":posts}
 
-@app.get("/posts")
+@app.get("/posts",response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
 
     posts=db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/posts",status_code=status.HTTP_201_CREATED)
-async def create_posts(post:Post,db: Session = Depends(get_db)):
+@app.post("/posts",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
+async def create_posts(post:schemas.PostCreate,db: Session = Depends(get_db)):
 
     new_post=models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
     
-@app.get("/posts/{id}")
-async def get_post(id:int):
+@app.get("/posts/{id}",response_model=schemas.Post)
+async def get_post(id:int,db: Session = Depends(get_db)):
 
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id),))
-    post=cursor.fetchone()
-
+    post= db.query(models.Post).filter(models.Post.id == id).first()
+    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post {id} was not found")
-    return {"post":post}
+    return post
 
 @app.delete("/posts/{id}")
-async def delete_post(id:int):
+async def delete_post(id:int,db: Session = Depends(get_db)):
 
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",(str(id),))
-    deleted_post=cursor.fetchone()
-    conn.commit()
+    post= db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post {id} was not found")
+    
+    post.delete(synchronize_session=False)
+    db.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-async def update_post(id:int,post:Post):
+@app.put("/posts/{id}",response_model=schemas.Post)
+async def update_post(id:int,updated_post:schemas.PostCreate,db: Session = Depends(get_db)):
+    
+    post_query= db.query(models.Post).filter(models.Post.id == id)
+    post=post_query.first()
 
-    cursor.execute("""UPDATE posts SET TITLE=%s, CONTENT=%s, PUBLISHED=%s WHERE ID=%s  RETURNING *""",(post.title,post.content,post.published,str(id)))
-    updated_post=cursor.fetchone()
-    conn.commit()
-
-    if updated_post== None:
+    if post== None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post {id} was not found")
-    return {"message":updated_post}
+
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+
+    return post_query.first()
+
+@app.post("/users",status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
+def create_user(user:schemas.UserCreate,db: Session = Depends(get_db)):
+
+    new_user=models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title:str
+    content:str
+    published:bool
+
+class PostCreate(PostBase):
+    published:bool=True
+    pass
+
+class Post(PostBase):
+    id:int 
+    created_at:datetime
+
+    class Config:
+        orm_mode=True
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+
+class UserOut(BaseModel):
+    id:int
+    email:EmailStr
+    created_at:datetime
+
+    class Config: 
+        orm_mode=True
